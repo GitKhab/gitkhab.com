ced4351cf859c62d442962fc425889bcab90d8e9
Vasil Khabibullin <gitkhab@yandex.ru>
2021-03-17 20:19:00 +0300

    Add logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="logo">
+    <span class="logo__sld">GitKhab</span>
+    <span class="logo__tld">.com</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Logo"
+  }
+</script>
+
+<style lang="scss">
+  @import "~@/assets/sass/variables.scss";
+
+  .logo {}
+
+  .logo__sld {
+    font-size: $font-size-xxl;
+    font-weight: 500;
+  }
+
+  .logo__tld {
+    font-size: $font-size-default;
+  }
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,18 @@
 <template>
   <header class="header">
-    <div class="header__inner">*header*</div>
+    <div class="header__inner">
+      <router-link class="header__link" to="/" title="Главная страница">
+        <Logo/>
+      </router-link>
+    </div>
   </header>
 </template>
 
 <script>
+  import Logo from "@/components/Logo";
   export default {
-    name: "TheHeader"
+    name: "TheHeader",
+    components: {Logo}
   }
 </script>
 
@@ -19,5 +25,15 @@
     max-width: $inner-max-width;
     margin: 0 auto;
     padding: 20px 16px;
+  }
+
+  .header__link {
+    display: inline-block;
+    text-decoration: none;
+    color: #fff;
+    transition: color 0.15s;
+    &:hover {
+      color: rgba(#fff, 0.75);
+    }
   }
 </style>

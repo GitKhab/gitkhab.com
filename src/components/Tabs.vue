2cc4c08e907f5882eb7eeefa3b37727a09254014
Vasil Khabibullin <gitkhab@yandex.ru>
2021-03-23 01:40:41 +0300

    Add tabs style

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,6 +2,7 @@
   <div class="tabs">
     <div class="tabs__header">
       <button class="tabs__tab"
+              :class="{ 'tabs__tab_active': tab.isActive }"
               v-for="tab in tabs"
               :key="tab.name"
               @click="selectTab(tab)">{{ tab.name }}</button>
@@ -32,3 +33,43 @@
     }
   }
 </script>
+
+<style lang="scss">
+  @import "~@/assets/sass/variables.scss";
+
+  $tabs-border-radius: 8px;
+
+  .tabs {}
+
+  .tabs__header {
+    padding: 0 8px;
+    border-top-left-radius: $tabs-border-radius;
+    border-top-right-radius: $tabs-border-radius;
+    background-color: rgba(#fff, 0.75);
+  }
+
+  .tabs__tab {
+    padding: 16px 8px;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
+    font-size: $font-size-m;
+    font-weight: 700;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    color: rgba(#000, 0.3);
+    &:hover {
+      color: $gray-dark;
+    }
+    &_active {
+      color: $gray-dark;
+    }
+  }
+
+  .tabs__body {
+    padding: 20px;
+    border-bottom-right-radius: $tabs-border-radius;
+    border-bottom-left-radius: $tabs-border-radius;
+    background-color: #fff;
+    color: $black-light;
+  }
+</style>

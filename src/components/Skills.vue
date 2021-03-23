938819ab7104e2aef2305d5e2b0d8a29e30ee456
Vasil Khabibullin <gitkhab@yandex.ru>
2021-03-23 14:40:43 +0300

    Add skill list

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,16 +3,35 @@
     <h1 class="skills__title">Навыки</h1>
     <Tabs>
       <Tab name="HTML" :selected="true">
-        <div>Навыки (HTML)</div>
+        <ul class="skills__list">
+          <li class="skills__list-item">Семантическая, доступная верстка</li>
+          <li class="skills__list-item">БЭМ-нотация</li>
+          <li class="skills__list-item">Pug</li>
+        </ul>
       </Tab>
       <Tab name="CSS">
-        <div>Навыки (CSS)</div>
+        <ul class="skills__list">
+          <li class="skills__list-item">Адаптивный дизайн (Flexbox, Grid, Media queries, etc.)</li>
+          <li class="skills__list-item">Препроцессоры (Sass, Stylus)</li>
+          <li class="skills__list-item">PostCSS</li>
+          <li class="skills__list-item">Bootstrap</li>
+        </ul>
       </Tab>
       <Tab name="JS">
-        <div>Навыки (JS)</div>
+        <ul class="skills__list">
+          <li class="skills__list-item">Ванильный JavaScript (ES5/6)</li>
+          <li class="skills__list-item">Vue (CLI, Router, Vuex)</li>
+          <li class="skills__list-item">Gulp (ленивые таски, инкрементальная сборка)</li>
+          <li class="skills__list-item">npm</li>
+        </ul>
       </Tab>
       <Tab name="Прочее">
-        <div>Прочие навыки</div>
+        <ul class="skills__list">
+          <li class="skills__list-item">Git (Flow)</li>
+          <li class="skills__list-item">VCS (GitHub, GitLab, Bitbucket)</li>
+          <li class="skills__list-item">Опыт работы с языками программирования: PHP, SQL, Python, С#, Prolog, Bash, 1С</li>
+          <li class="skills__list-item">Опыт работы с программным обеспечением: JetBrains IDE (WebStorm, PhpStorm, etc.), Adobe (Photoshop, Illustrator), Figma, ftp-клиенты</li>
+        </ul>
       </Tab>
     </Tabs>
   </section>
@@ -38,5 +57,16 @@
     font-size: $font-size-xl;
     font-weight: 500;
     color: #fff;
+  }
+
+  .skills__list {
+    margin: 0;
+    padding-inline-start: 20px;
+  }
+
+  .skills__list-item {
+    &:not(:last-child) {
+      margin-bottom: 8px;
+    }
   }
 </style>

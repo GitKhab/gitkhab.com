051dbbbb3246ce6b76926a26f9f09eaeb5d2f90b
Vasil Khabibullin <gitkhab@yandex.ru>
2021-03-28 14:57:13 +0300

    Update app structure
    Новая структура позволяет создавать представления снаружи базового.
    При желании это позволит, например, создать независимую страницу
    ошибки.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,11 @@
 <template>
   <div id="app">
-    <div class="app__inner">
-      <TheHeader class="app__header"/>
-      <router-view class="app__main"/>
-    </div>
-    <TheFooter class="app__footer"/>
+    <router-view/>
   </div>
 </template>
-<script>
-  import TheHeader from "@/components/TheHeader";
-  import TheFooter from "@/components/TheFooter";
 
-  export default {
-    components: {TheFooter, TheHeader}
-  }
-</script>
-
-<style lang="scss">
-  @import "~@/assets/fonts/roboto/roboto.css";
-  @import "~@/assets/sass/variables.scss";
-
-  html {
-    height: 100%;
-  }
-
-  body {
-    height: inherit;
-    min-width: 320px;
-    margin: 0;
-    font-family: $fonts-default;
-    line-height: 1.4;
-    background-color: $main-dark;
-  }
-
+<style>
   #app {
-    display: flex;
-    flex-direction: column;
     height: inherit;
-  }
-
-  .app__inner {
-    flex-grow: 1;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .app__header {
-    flex-shrink: 0;
-  }
-
-  .app__main {
-    flex-grow: 1;
-  }
-
-  .app__footer {
-    flex-shrink: 0;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import BaseLayout from "@/views/BaseLayout";
+import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      title: 'Резюме на позицию «junior frontend-разработчик»'
-    }
+    component: BaseLayout,
+    children: [
+      {
+        path: '',
+        name: Home,
+        component: Home
+      }
+    ]
   }
 ]
 

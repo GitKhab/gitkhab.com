7203058dc9041e45cfc9f6997966f741b5f27950
Vasil Khabibullin <gitkhab@yandex.ru>
2021-03-30 15:11:27 +0300

    Add person module

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import website from '@/store/modules/website'
-import getAge from "@/assets/js/getAge";
+import person from '@/store/modules/person'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    person: {
-      firstName: 'Василь',
-      lastName: 'Хабибуллин',
-      birthday: '1995-09-11',
-      email: 'gitkhab@yandex.ru',
-      telegram: 'https://t.me/GitKhab',
-      github: 'https://github.com/GitKhab'
-    }
-  },
-  getters: {
-    getPersonFullName(state) {
-      return `${state.person.lastName} ${state.person.firstName}`;
-    },
-    getPersonAge(state) {
-      return getAge(state.person.birthday);
-    }
-  },
-  mutations: {},
-  actions: {},
   modules: {
-    website
+    website,
+    person
   }
 })

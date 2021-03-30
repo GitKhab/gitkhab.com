95819745c4c4bc8e2979ce6cccb5cffc3bafbe35
Vasil Khabibullin <gitkhab@yandex.ru>
2021-03-30 17:47:53 +0300

    Add education module

--- src/components/AppEducation.vue
+++ src/components/AppEducation.vue
@@ -10,40 +10,9 @@
 <script>
   export default {
     name: "AppEducation",
-    data() {
-      return {
-        education: [
-          {
-            id: 1,
-            label: 'Год',
-            content: '2014-2016 (неполное высшее)'
-          },
-          {
-            id: 2,
-            label: 'ВУЗ',
-            content: 'МГТУ «СТАНКИН»'
-          },
-          {
-            id: 3,
-            label: 'Институт',
-            content: 'информационных систем и технологий'
-          },
-          {
-            id: 4,
-            label: 'Кафедра',
-            content: 'информационных систем'
-          },
-          {
-            id: 5,
-            label: 'Отделение',
-            content: 'очное'
-          },
-          {
-            id: 6,
-            label: 'Код направления',
-            content: '09.03.02'
-          }
-        ]
+    computed: {
+      education() {
+        return this.$store.state.education;
       }
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import website from '@/store/modules/website'
 import person from '@/store/modules/person'
 import resume from '@/store/modules/resume'
+import education from '@/store/modules/education'
 
 Vue.use(Vuex)
 
@@ -10,6 +11,7 @@ export default new Vuex.Store({
   modules: {
     website,
     person,
-    resume
+    resume,
+    education
   }
 })

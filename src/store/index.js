a9c5a6cb0756eada5b9035c5feae7cb90e2daad6
Vasil Khabibullin <gitkhab@yandex.ru>
2021-03-30 17:41:36 +0300

    Add resume module

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import website from '@/store/modules/website'
 import person from '@/store/modules/person'
+import resume from '@/store/modules/resume'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     website,
-    person
+    person,
+    resume
   }
 })
